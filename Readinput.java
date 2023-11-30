13d54b885fb22411e912f471788ed32e313bc8f6
Vyshnavi0226 <152470995+Vyshnavi0226@users.noreply.github.com>
2023-11-30 11:34:32 +0530

    30/11/2023

---
+++ Readinput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ReadInput
+{
+   public static void main(String args{})
+   {
+      System.ou.println("\nEnter a number = ");
+      Scanner reader = new Scanner(System.in);
+      int num = reader.nextInt();
+      System.out.println("\nEntered no is" + num);
+   }
+}dir
+ddir
